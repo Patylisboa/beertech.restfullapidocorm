93048c22d98853d8bd09c5c5229c889893a5cec3
Victor Hugo Rodrigues G. de Carvalho <vcarvalho@cpqd.com.br>
2020-10-06 20:09:09 -0300

    Estrutura inicial

---
+++ src/main/java/beertech/kpoc/KPocApplication.java
@@ -0,0 +1,13 @@
+package beertech.kpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/beertech/kpoc/KPocApplicationTests.java
@@ -0,0 +1,13 @@
+package beertech.kpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

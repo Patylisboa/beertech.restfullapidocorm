bcaa65eafd647c82cd34d52ebea515f67c9da911
jeovane <jeovanebc@gmail.com>
2020-10-06 21:17:36 -0300

    merge

--- src/main/java/com/beertech/kpoc/controllers/ORMController.java
+++ src/main/java/com/beertech/kpoc/controllers/ORMController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.xml.ws.Response;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;

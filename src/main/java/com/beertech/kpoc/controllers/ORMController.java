42deead37f64335d03925b722de4243e361e1c0b
Victor Hugo Rodrigues G. de Carvalho <vcarvalho@cpqd.com.br>
2020-10-06 21:01:06 -0300

    Metodos get e post basicos

--- src/main/java/com/beertech/kpoc/controllers/ORMController.java
+++ src/main/java/com/beertech/kpoc/controllers/ORMController.java
@@ -1,9 +1,36 @@
 package com.beertech.kpoc.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.beertech.kpoc.entities.ORM;
+import com.beertech.kpoc.repositories.ORMRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/orms")
 public class ORMController {
+	@Autowired
+	private ORMRepository repository;
+
+
+
+	@GetMapping
+	public List<ORM> findAllORMs() {
+		return repository.findAll();
+	}
+
+	@GetMapping("/orm/{name}")
+	public ORM findORMByName(@PathVariable String name) {
+		Optional<ORM> orm = repository.findByName(name);
+		return orm.get();
+	}
+
+	@PostMapping("/orms")
+	public ResponseEntity<ORM> createORM(@RequestBody ORM orm) {
+		ORM persistedORM = repository.save(orm);
+		return ResponseEntity.ok(persistedORM);
+	}
 }
--- src/main/java/com/beertech/kpoc/repositories/ORMRepository.java
+++ src/main/java/com/beertech/kpoc/repositories/ORMRepository.java
@@ -3,5 +3,9 @@ package com.beertech.kpoc.repositories;
 import com.beertech.kpoc.entities.ORM;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ORMRepository extends JpaRepository<ORM, Long> {
+
+	Optional<ORM> findByName(String name);
 }
